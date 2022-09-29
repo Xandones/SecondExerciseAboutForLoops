b328bf4a68ddcc74de1dcc0e2db5d5f0e3f6d293
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-09-29 08:32:59 -0300

    First commit

---
+++ app/src/main/java/com/alexandrohlopes/secondexerciseaboutforloops/secondForExercise.kt
@@ -0,0 +1,10 @@
+package com.alexandrohlopes.secondexerciseaboutforloops
+
+fun main() {
+    for (x in 0..10000) {
+        if (x == 9001) {
+            print("IT'S OVER 9000!")
+        }
+        println(x)
+    }
+}
